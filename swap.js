6b7d9958805e4994a2e08e6c619e6e11fb76033c
zubiyabintamostofa <zubiyabinte123@gmail.com>
2022-02-02 00:57:18 +0600

    swap

---
+++ swap.js
@@ -0,0 +1,17 @@
+var first=9;
+var second=22;
+console.log(first,second);
+var temp=first;
+first=second;
+second=temp;
+console.log(first,second);
+//swap mane man ekchange hobe.
+
+
+//desstructing
+var first=9;
+var second=22;
+[first,second]=[second,first];
+console.log(first,second);
+
+
