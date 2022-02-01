5de450d9d848e209175b3393600b0dc6b22a3b6a
zubiyabintamostofa <zubiyabinte123@gmail.com>
2022-02-02 00:33:32 +0600

    new

---
+++ math.js
@@ -0,0 +1,22 @@
+const mynum=-5;
+const output=Math.abs(mynum);
+console.log(output);
+//dosomik er porer punno songkha pete chaile ai niyom
+const hernum=39.889;
+const ans=Math.ceil(hernum);
+console.log(ans);
+//dosomik er ager punno songkha pete chaile ei noyom
+const hisnum=39.889;
+const reply=Math.ceil(hisnum);
+console.log(reply);
+// nearest punno songkhar niyom:
+
+const hernum2=42.889;
+const paici=Math.round(hernum2);
+console.log(paici);
+
+//random punnosongkha pete cheile:
+
+const hernum3=39.889;
+const ans2=Math.random()*10;
+console.log(ans2);
