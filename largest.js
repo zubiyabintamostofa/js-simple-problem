1d898b48d41b644c96bf1878cf992d3fd2495e25
zubiyabintamostofa <zubiyabinte123@gmail.com>
2022-02-02 13:20:08 +0600

    array

---
+++ largest.js
@@ -0,0 +1,13 @@
+function largestElement(num){
+    let largest=0;
+    for(let i=0;i<num.length;i++){
+        const element=num[i];
+        if(element>largest){
+            largest=element;
+        }
+        
+    }return largest;
+}
+const ages=[12,34,56,123,034,321];
+const oldest=largestElement(ages);
+console.log(oldest);
