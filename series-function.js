39d0c1e308c255f100e2d9fe03d99cf0e1d41fe8
zubiyabintamostofa <zubiyabinte123@gmail.com>
2022-02-03 19:33:13 +0600

    bakke

--- series-function.js
+++ series-function.js
@@ -1,9 +1,17 @@
 function fibonacciSeries(num){
+    //ei line ta dewa hoiche cz amra kuno karone string likhle eita output asbe
+    if(typeof num!='number'){
+        return 'please give a number'
+    }
     const fibo=[0,1]
     for(i=2;i<=num;i++){
         fibo[i]=fibo[i-1]+fibo[i-2];
     }
     return fibo;
 }
-const fiboSeries=fibonacciSeries(13);
+
+    
+
+const fiboSeries=fibonacciSeries(12);
 console.log(fiboSeries);
+//
